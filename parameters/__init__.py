2f55d71f13356334d0642b737cb377671960ad1f
AurelienNioche <nioche.aurelien@gmail.com>
2017-12-06 18:13:23 +0100

    Various updates

--- parameters/__init__.py
+++ parameters/__init__.py
@@ -2,13 +2,12 @@ from . parameters import *
 
 
 def items():
-
-    return dict([(i, j) for i, j in parameters.__dict__.items() if not i.startswith("__")])
+    return [(i, j) for i, j in parameters.__dict__.items() if not i.startswith("__")]
 
 
 class Parameters:
 
     def __init__(self):
 
-        for key, value in items().items():
+        for key, value in items():
             setattr(self, key, value)
